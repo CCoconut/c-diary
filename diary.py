8ea9f5de57f3b05b489d0a124b089b6a73d9116d
CCoconut <CCoconut@users.noreply.github.com>
2016-09-20 23:26:29 -0400

    Update diary.py

--- diary.py
+++ diary.py
@@ -6,14 +6,14 @@ program_open = 1
 
 # This function writes input data to a text file with today's date as a filename and header
 def diary_write():
-	diary_entry = open(str(date) + ".txt", "w");
+	diary_entry = open(str(date), "w");
 	diary_entry.write(str(date) + "\n");
 	dream = input("Write your entry here: \n");
 	diary_entry.write(dream);
 	diary_entry.close();
 
 def diary_log():
-	diary_log = open("Entry Dates.txt", "w");
+	diary_log = open("Entry Dates", "w");
 	diary_log.write(str(date) + "\n");
 	diary_log.close();
 
@@ -21,7 +21,7 @@ def diary_log():
 # only able to read files with YYYY-MM-DD as a filename format.
 def diary_read():
 	while answer == 'read':
-		read_entry_yes = input("\nWhat is the date of the entry? (yyyy-mm-yy) If you want to stop reading, type stop: ");
+		read_entry_yes = input("\nWhat is the date of the entry? (yyyy-mm-dd) If you want to stop reading, type stop: ");
 		if read_entry_yes == "stop":
 			break
 		elif os.path.lexists(read_entry_yes) == False:
@@ -31,6 +31,7 @@ def diary_read():
 			diary_read = open(read_entry_yes, "r");
 			print("\n" + diary_read.read());
 			diary_read.close();
+			continue
 
 # Version 0.2.1: Now asks for a command instead of if the user dreamt. This is to make it more general than
 # just a dream journal (can be used as any kind of diary now). The while loop ensures that it will always
@@ -39,7 +40,7 @@ while program_open == 1:
 	answer = input("\nWhat do you want to do? (Type 'help' for a list of commands): ");
 	if answer == 'write':
 		date = datetime.date.today();
-		if os.path.lexists(str(date) + ".txt") == True:
+		if os.path.lexists(str(date)) == True:
 			print("\nYou already wrote an entry for today!");
 		else:
 			diary_write();
@@ -49,11 +50,10 @@ while program_open == 1:
 	elif answer == 'help':
 		print("\nValid commands are: " + "\n'write' (write an entry)" + "\n'read' (read entries)" + "\n'help' (display this menu)" + "\n'entries' (see what dates have entries)" + "\n'quit'");
 	elif answer == 'entries':
-		entry_dates = open("Entry Dates.txt", "r");
+		entry_dates = open("Entry Dates", "r");
 		print("\n" + entry_dates.read());
-	elif answer == 'quit':
+	elif answer == 'quit' or 'exit':
 		break
 	else:
 		print("\nThat is not a valid command.");
 print("\nHave a nice day!\n");
-
