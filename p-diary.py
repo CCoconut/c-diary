6998914554f0d1040777ad2d5d2a6f202436eb96
CCoconut <CCoconut@users.noreply.github.com>
2016-10-11 20:55:36 -0400

    Version 0.3

--- p-diary.py
+++ p-diary.py
@@ -1,35 +1,49 @@
-#!/usr/bin/env python
-import os.path;
+#/!/usr/bin/env python
+import os;
 import datetime;
 
-# Version 0.2.2: P-Diary will now always write and read files to/from whatever directory the script is in.
+# Version 0.3: P-Diary now supports multiple diaries. Each one is its own directory. Code hasn't been cleaned up yet.
 
 program_open = 1;
 directory = os.path.join(os.path.expanduser('~'),'Diary');
 
+def create_directory():
+	new_diary = input("Name your diary (type 'cancel' to go back): \n");
+	if new_diary == "cancel":
+		print("\nOkay.");
+	else:
+		os.mkdir(os.path.join(directory,new_diary));
+		new_diary_log = open(os.path.join(directory, "Diaries"), "a");
+		new_diary_log.write(new_diary + "\n");
+		new_diary_log.close();
+
 # This function writes input data to a text file with today's date as a filename and header
 def diary_write():
-	diary_entry = open(os.path.join(directory, str(date)), "w");
+	diary_entry = open(os.path.join(set_directory, str(date)), "w");
 	diary_entry.write(str(date) + "\n");
 	dream = input("Write your entry here: \n");
 	diary_entry.write(dream);
 	diary_entry.close();
 
+def Diaries():
+	see_diaries = open(os.path.join(directory, "Diaries"), "r");
+	print("\n" + see_diaries.read());
+
 # This function writes an entry date to a file that can be called up as a reminder
 def diary_log():
-	diary_log = open(os.path.join(directory, "Entry Dates"), "a");
+	diary_log = open(os.path.join(set_directory, "Entry Dates"), "a");
 	diary_log.write(str(date) + "\n");
 	diary_log.close();
 
 # This function lets you read the entry dates file to see which dates have entries
 def diary_entries():
-	entry_dates = open(os.path.join(directory, "Entry Dates"), "r");
+	entry_dates = open(os.path.join(set_directory, "Entry Dates"), "r");
 	print("\n" + entry_dates.read());
 
 # This function reads text file contents and prints it for the user to read. Need to find a way to make it
-# only able to read files with YYYY-MM-DD as a filename format.
+# only able to read files with YYYY-MM-DD as a filename format. (does not yet do regex)
 def diary_read():
-	while answer == 'read':
+	while answer2 == 'read':
 		read_entry_yes = input("\nWhat is the date of the entry? (Type 'help' to see other commands): ");
 		if read_entry_yes == "stop":
 			break
@@ -37,39 +51,55 @@ def diary_read():
 			diary_entries();
 		elif read_entry_yes == "help":
 			print("\nValid commands are: " + "\n'help' (display this menu)" + "\n'entries' (see what dates have entries)" + "\n'stop' (stop reading entries)" + "\nInput an entry date to read it");
-		elif os.path.lexists(os.path.join(directory, read_entry_yes)) == False:
+		elif os.path.lexists(os.path.join(set_directory, read_entry_yes)) == False:
 			print("\nYou didn't write an entry that day (or you typed an invalid command!)");
 			continue
 		else:
-			diary_read = open(os.path.join(directory, read_entry_yes), "r");
+			diary_read = open(os.path.join(set_directory, read_entry_yes), "r");
 			print("\n" + diary_read.read());
 			diary_read.close();
 			continue
 
-# The while loop ensures that it always goes back to 'What do you want to do?' unless the user
-# exits the program.
+# Main body of program. Asks for which diary to look at then gives the option to read, see entries, and write)
 while program_open == 1:
-	answer = input("\nWhat do you want to do? (Type 'help' for a list of commands): ");
-	if answer == 'write':
-		date = datetime.date.today();
-		if os.path.lexists(os.path.join(directory, str(date))) == True:
-			print("\nYou already wrote an entry for today!");
-		else:
-			diary_write();
-			diary_log();
-	elif answer == 'read':
-		diary_read();
-	elif answer == 'help':
-		print("\nValid commands are: " + "\n'write' (write an entry)" + "\n'read' (read entries)" + "\n'help' (display this menu)" + "\n'entries' (see what dates have entries)" + "\n'quit'");
-	elif answer == 'entries':
-		diary_entries();
-	elif answer == 'quit' or answer == 'exit':
+	answer1 = input("\nWhich diary do you want to look at? (type 'help' for commands) ");
+	if answer1 == "new":
+		create_directory();
+	elif answer1 == "help":
+		print("\nValid commands are: " + "\n'new' (create a new diary)" + "\n'diaries' (see a list of your diaries)" + "\n'help' (display this menu)" + "\n'quit' (exit)");
+	elif answer1 == "diaries":
+		Diaries();
+	elif answer1 == "quit" or answer1 =="exit":
 		break
 	else:
-		print("\nThat is not a valid command.");
+		global set_directory
+		set_directory = os.path.join(directory,answer1);
+		if os.path.exists(set_directory) == False:
+			print("\nThat diary doesn't exist!");
+			continue
+		else:
+			while os.path.exists(set_directory) == True:
+				answer2 = input("\nWhat do you want to do? (Type 'help' for a list of commands): ");
+				if answer2 == 'write':
+					date = datetime.date.today();
+					if os.path.lexists(os.path.join(set_directory, str(date))) == True:
+						print("\nYou already wrote an entry in this diary today!");
+					else:
+						diary_write();
+						diary_log();
+				elif answer2 == 'read':
+					diary_read();
+				elif answer2 == 'help':
+					print("\nValid commands are: " + "\n'write' (write an entry)" + "\n'read' (read entries)" + "\n'help' (display this menu)" + "\n'entries' (see what dates have entries)" + "\n'quit'");
+				elif answer2 == 'entries':
+					diary_entries();
+				elif answer2 == 'quit' or answer2 == 'exit':
+					break
+				else:
+					print("\nThat is not a valid command.");
 print("\nHave a nice day!\n");
 
 # List of bugs or changes I need to implement:
 # Confirmation before writing entry
-# Deleting file if it's not written to
-# Calendar
+# Deleting file if it's not written to (i.e., if the user interrupts the program with ctrl-c)
+# Calendars
