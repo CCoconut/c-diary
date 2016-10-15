18c397615c9e41e15d393dd82ed1cecb124800eb
CCoconut <CCoconut@users.noreply.github.com>
2016-10-15 16:13:08 -0400

    0.3.2

--- p-diary.py
+++ p-diary.py
@@ -1,7 +1,7 @@
 #/!/usr/bin/env python
 import os, datetime, textwrap;
 
-# Version 0.3.1: p-diary now supports multiple diaries. Each one is its own directory.
+# Version 0.3.2: p-diary now supports multiple diaries. Each one is its own directory. Also asks for confirmation before sending an entry.
 
 directory = os.path.join(os.path.expanduser('~'),'p-diary');
 program_open = 1;
@@ -37,16 +37,30 @@ def diary_log():
 
 # This function writes input data to a text file with today's date as a filename and header
 def diary_write():
+	global diary_entry;
 	diary_entry = open(os.path.join(set_directory, str(date)), "w");
+	global dream;
 	dream = input("\nWrite your entry here (type 'cancel' and press enter to go back): \n");
 	if dream == 'cancel':
 		diary_entry.close();
 		os.remove(os.path.join(set_directory, str(date)));
 	else:
+		diary_write_confirm();
+
+# This function is for confirmation of writing an entry
+def diary_write_confirm():
+	confirm = input("\nDo you want to write this entry? (y/n): ");
+	if confirm.lower()[0] == 'y':
 		diary_entry.write("(" + str(date) + ")" + "\n");
 		diary_entry.write(dream);
 		diary_entry.close();
 		diary_log();
+	elif confirm.lower()[0] == 'n':
+		diary_entry.close();
+		os.remove(os.path.join(set_directory, str(date)));
+	else:
+		print("\nPlease use 'y' or 'n'.");
+		diary_write_confirm();
 
 # This function lets you read the entry dates file to see which dates have entries
 def diary_entries():
@@ -64,19 +78,16 @@ def diary_read():
 			diary_entries();
 		elif read_entry_yes == "help":
 			print("\nValid commands are: " + "\n'help' (display this menu)" + "\n'entries' (see what dates have entries)" + "\n'today' (read today's entry)" + "\n'exit' (stop reading entries)" + "\nInput an entry date to read it");
-		elif read_entry_yes == "today":
+		elif read_entry_yes == "today" and os.path.lexists(os.path.join(set_directory, str(datetime.date.today()))) == True:
 			diary_read_today = open(os.path.join(set_directory, str(datetime.date.today())), "r");
 			print("\n" + textwrap.fill(diary_read_today.read(), 80));
 			diary_read_today.close();
-			continue
 		elif os.path.lexists(os.path.join(set_directory, read_entry_yes)) == False:
 			print("\nYou didn't write an entry that day (or you typed an invalid command!)");
-			continue
 		else:
 			diary_read = open(os.path.join(set_directory, read_entry_yes), "r");
 			print("\n" + textwrap.fill(diary_read.read(), 80));
 			diary_read.close();
-			continue
 
 # Main body of program. Asks for which diary to look at then gives the option to read, see entries, and write)
 while program_open == 1:
@@ -90,11 +101,10 @@ while program_open == 1:
 	elif answer1 == "quit" or answer1 =="exit":
 		break
 	else:
-		global set_directory
+		global set_directory;
 		set_directory = os.path.join(directory,answer1);
 		if os.path.exists(set_directory) == False:
 			print("\nThat diary doesn't exist!");
-			continue
 		else:
 			while os.path.exists(set_directory) == True:
 				answer2 = input("\nWhat do you want to do? (Type 'help' for a list of commands): ");
@@ -117,6 +127,6 @@ while program_open == 1:
 print("\nHave a nice day!\n");
 
 # List of bugs or changes I need to implement:
-# Confirmation before writing entry
-# Deleting file if it's not written to (i.e., if the user interrupts the program with ctrl-c)
+# Confirmation doesn't take you back to edit your entry. I want to figure out a way to do this.
+# Better entry management - right now you can only access files by typing in their date or 'today'. I'd like to implement entry numbers to make it easier.
 # Calendars
